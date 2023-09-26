f70d7afd1f380ead68bf70cd8759945849932603
Sgonzalez <Sgonzalez@106-14.info2>
2023-09-26 13:40:10 +0200

    Actualización

--- src/tema1/Ejercicio5.java
+++ src/tema1/Ejercicio5.java
@@ -4,16 +4,29 @@ import java.util.Scanner;
 
 public class Ejercicio5 {
 	public static void main(String[] args) {
-		// Declaro variables
-		double longitud, area, radio;
-		Scanner sc = new sc(System.in);
-		// Intrucciones
+		// Variables donde guardar el radio.
+		double radio;
+		//Variable donde guardar la longitud.
+		double longitud;
+		//Variable donde guardar el área.
+		double area;
+		//Creo un Scanner para leer el teclado.
+		Scanner sc = new Scanner(System.in);
+		// Le pido al usuario que introduzca un radio.
 		System.out.println("Introduce el radio:");
+		//Leemos el radio del teclado.
 		radio = sc.nextDouble();
+		//Calculamos la longitud
 		longitud = (2*Math.PI*radio);
-		area = (Math.PI* (radio, 2);
+		//Calculamos el Área.
+		area = (Math.PI*Math.pow(radio,2));
+		//Mostramos la longitud en la consola.
 		System.out.println("La longitud es: "+ longitud);
+		//Mostramos ell área en la consola.
 		System.out.println("El área es: " + area);
+		//cerramos el Scanner
+		sc.close();
+		
 	}
 
 }
---
+++ src/tema1/Ejercicio6.java
@@ -0,0 +1,18 @@
+package tema1;
+
+import java.util.Scanner;
+
+public class Ejercicio6 {
+	public static void main(String[] args) {
+		//Variable donde guardar el primer número.
+		int numero1;
+		//Variable donde guardar el segundo número.
+		int numero2;
+		//Creo el escaner.
+	Scanner Scanner = new Scanner (System.in);
+	//Pedimos al usuario que introduzca el primer número.
+	System.out.println("Introduce un número");
+	//Leemos 
+	
+}
+}
