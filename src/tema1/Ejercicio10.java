5b91076d69ae2120dc07aa36e090bfb1066dca68
Sergiognlz <sergio.gonzalez@iesnervion.es>
2023-10-20 14:37:11 +0200

    Complete

--- src/tema1/Ejercicio10.java
+++ src/tema1/Ejercicio10.java
@@ -1,5 +1,5 @@
 package tema1;
-
+//Importamos el escaner de java
 import java.util.Scanner;
 
 public class Ejercicio10 {
@@ -14,6 +14,9 @@ public static void main(String[] args) {
 	System.out.println("Introduce un precio en euros");
 	//Leemos el precio
 	precio= sc.nextDouble();
-	
+	//Mostramos el valor en euros del precio con el IVA añadido
+	System.out.println("Tu precio más IVA es: " + precio*21/100 );
+	//Cerramos el escaner
+	sc.close();
 }
 }
